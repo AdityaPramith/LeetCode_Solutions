a9c5f5c26facd5160333d7abf2341efde7d28111
Aditya Pramith <adityapramith@gmail.com>
2025-04-16 23:46:29 +0530

    Update 2537CountNumberOgGoodSubArrays.java

--- Java/2537CountNumberOgGoodSubArrays.java
+++ Java/2537CountNumberOgGoodSubArrays.java
@@ -1,15 +1,11 @@
 class Solution {
     public long countGood(int[] nums, int k) {
         Map<Integer, Integer> freq = new HashMap<>();
-        int left = 0;
-        long pairCount = 0;
-        long goodSubarrays = 0;
-
+        int left = 0,pairCount = 0,goodSubarrays = 0;
         for (int right = 0; right < nums.length; right++) {
             int count = freq.getOrDefault(nums[right], 0);
             pairCount += count;
             freq.put(nums[right], count + 1);
-
             while (pairCount >= k) {
                 goodSubarrays += nums.length - right;
                 int leftNum = nums[left++];
